634e0f5713b185ebbd6ed2e0f387c0be9599ca51
RyutoShimada <business.sub10110@gmail.com>
2021-08-07 17:29:46 +0900

    Playerの動きと弾の処理、爆発の処理を確認

--- Assets/Scripts/Shimada/Player.cs
+++ Assets/Scripts/Shimada/Player.cs
@@ -3,7 +3,7 @@ using System.Collections.Generic;
 using UnityEngine;
 
 [SerializeField]
-enum OperaterState
+public enum OperaterState
 {
     FirstPlayer,
     SecondPlayer
@@ -16,6 +16,11 @@ public class Player : MonoBehaviour
     [SerializeField] int m_hp = 10;
     [SerializeField] int m_damage = 1;
     [SerializeField] float m_speed = 5f;
+
+    [SerializeField] GameObject m_bulletPrefab = null;
+    [SerializeField] Transform m_bulletGeneratePos = null;
+
+    public OperaterState m_operater = OperaterState.FirstPlayer;
     Rigidbody2D m_rb2d;
     Vector2 m_velo;
     Vector2 m_dir;
@@ -28,10 +33,15 @@ public class Player : MonoBehaviour
         m_dir = Vector2.up;
     }
 
-    // Update is called once per frame
-    void Update()
+    void FixedUpdate()
     {
         Move();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {   
+        Fire();
     }
 
     private void Move()
@@ -39,28 +49,53 @@ public class Player : MonoBehaviour
         float h = 0;
         float v = 0;
 
-        if (Input.GetKey(KeyCode.W))
+        if (m_operater == OperaterState.FirstPlayer)
         {
-            v = 1f;
+            if (Input.GetKey(KeyCode.W))
+            {
+                v = 1f;
+            }
+            if (Input.GetKey(KeyCode.S))
+            {
+                v = -1f;
+            }
+            if (Input.GetKey(KeyCode.A))
+            {
+                h = -1f;
+            }
+            if (Input.GetKey(KeyCode.D))
+            {
+                h = 1f;
+            }
+            if (Input.GetKeyUp(KeyCode.W) || Input.GetKeyUp(KeyCode.A) || (Input.GetKeyUp(KeyCode.S) || Input.GetKeyUp(KeyCode.D)))
+            {
+                v = 0;
+                h = 0;
+            }
         }
-        if (Input.GetKey(KeyCode.S))
+        else
         {
-            v = -1f;
-        }
-
-        if (Input.GetKey(KeyCode.A))
-        {
-            h = -1f;
-        }
-        if (Input.GetKey(KeyCode.D))
-        {
-            h = 1f;
-        }
-
-        if (Input.GetKeyUp(KeyCode.W) || Input.GetKeyUp(KeyCode.A) || (Input.GetKeyUp(KeyCode.S) || Input.GetKeyUp(KeyCode.D)))
-        {
-            v = 0;
-            h = 0;
+            if (Input.GetKey(KeyCode.UpArrow))
+            {
+                v = 1f;
+            }
+            if (Input.GetKey(KeyCode.DownArrow))
+            {
+                v = -1f;
+            }
+            if (Input.GetKey(KeyCode.LeftArrow))
+            {
+                h = -1f;
+            }
+            if (Input.GetKey(KeyCode.RightArrow))
+            {
+                h = 1f;
+            }
+            if (Input.GetKeyUp(KeyCode.UpArrow) || Input.GetKeyUp(KeyCode.DownArrow) || (Input.GetKeyUp(KeyCode.LeftArrow) || Input.GetKeyUp(KeyCode.RightArrow)))
+            {
+                v = 0;
+                h = 0;
+            }
         }
 
         m_dir = (Vector2.up * v + Vector2.right * h).normalized;
@@ -74,7 +109,31 @@ public class Player : MonoBehaviour
         {
             m_rb2d.velocity = Vector2.zero;
         }
+    }
 
+    void Fire()
+    {
         
+
+        if (m_operater == OperaterState.FirstPlayer)
+        {
+            if (Input.GetKeyDown(KeyCode.LeftControl))
+            {
+                Debug.Log($"{m_operater} : Fire!");
+                if (!m_bulletPrefab) return;
+
+                Instantiate(m_bulletPrefab, m_bulletGeneratePos.position, m_bulletGeneratePos.rotation);
+            }
+        }
+        else
+        {
+            if (Input.GetKeyDown(KeyCode.RightControl))
+            {
+                Debug.Log($"{m_operater} : Fire!");
+                if (!m_bulletPrefab) return;
+
+                Instantiate(m_bulletPrefab, m_bulletGeneratePos.position, m_bulletGeneratePos.rotation);
+            }
+        }
     }
 }
