74076b602f281d8e1b49b80b349ad83dd415aa2f
DESKTOP-4NV9MP5\vantan <sskryo0607@gmail.com>
2021-08-08 14:40:55 +0900

    爆弾のオブジェクトプール化、効果音付け

--- Assets/Scripts/Sasaki/Bomb.cs
+++ Assets/Scripts/Sasaki/Bomb.cs
@@ -10,30 +10,45 @@ public class Bomb : MonoBehaviour
     //爆破弾
     [SerializeField] private GameObject m_bombBullet = default;
 
-    [SerializeField] float m_waitForSeconds = 0.5f;
+    [SerializeField] private float m_waitForSeconds = 0.5f;
 
     //爆破弾の最大値
-    [SerializeField] int m_bombShot = 10;
+    [SerializeField] private int m_bombShot = 10;
 
     //爆破弾層
-    private int m_bombShotMilfiyu = 3;
+    private int m_bombShotMilfiyu = 4;
 
-    void Start()
+    //爆弾の当たり判定
+    private CircleCollider2D m_bombCollision;
+
+    //爆弾の絵
+    SpriteRenderer m_sprite = null;
+
+    //爆発アニメーションのフィールド
+    GameObject m_exprosion = null;
+
+    private void Awake()
     {
-        
+        m_bombCollision = this.GetComponent<CircleCollider2D>();
+        m_sprite = GetComponent<SpriteRenderer>();
     }
 
-    void Update()
+
+    private void OnEnable()
     {
-        
+        m_bombCollision.enabled = true;
+        m_sprite.enabled = true;
     }
 
     private void OnCollisionEnter2D(Collision2D collision)
     {
         if(collision.gameObject.tag == "Bullet")
         {
+            m_bombCollision.enabled = false;
+            m_sprite.enabled = false;
+
             //爆発アニメーション生成
-            Instantiate(m_bomberAnime,
+            m_exprosion = Instantiate(m_bomberAnime,
                 transform.position,
                 transform.rotation);
 
@@ -41,7 +56,7 @@ public class Bomb : MonoBehaviour
             //爆破弾生成
             for (var i = 1; i <= m_bombShot; i++)
             {
-                Instantiate(m_bombBullet,
+               Instantiate(m_bombBullet,
                transform.position,
                Quaternion.AngleAxis(Random.Range(0, 360), Vector3.forward));
 
@@ -58,7 +73,7 @@ public class Bomb : MonoBehaviour
     IEnumerator Generate()
     {
         yield return new WaitForSeconds(m_waitForSeconds);
-        for (var n = 0; n < 4; n++)
+        for (var n = 0; n < m_bombShotMilfiyu; n++)
         {
             for (var x = 1; x <= m_bombShot; x++)
             {
@@ -69,6 +84,7 @@ public class Bomb : MonoBehaviour
             }
             yield return new WaitForSeconds(m_waitForSeconds);
         }
-        Destroy(gameObject);
+
+        this.gameObject.SetActive(false);
     }
 }
--- Assets/Scripts/Sasaki/ExprosionDestroy.cs
+++ Assets/Scripts/Sasaki/ExprosionDestroy.cs
@@ -4,7 +4,7 @@ using UnityEngine;
 
 public class ExprosionDestroy : MonoBehaviour
 {
-    private void AfterExprosion()
+    public void AfterExprosion()
     {
         Destroy(this.gameObject);
     }
